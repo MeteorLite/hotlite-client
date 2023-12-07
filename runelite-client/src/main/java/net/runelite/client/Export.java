10d938808d4ddd7afe992eea838749c36f962a5c
Tyler Bochard <zeruth09@gmail.com>
2023-12-07 02:59:07 -0500

    cleanup Export
    fixes rl version in some situations

--- runelite-client/src/main/java/net/runelite/client/Export.java
+++ runelite-client/src/main/java/net/runelite/client/Export.java
@@ -1,7 +1,5 @@
 package net.runelite.client;
 
-import net.runelite.http.api.RuneLiteAPI;
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -9,17 +7,26 @@ import java.nio.file.StandardCopyOption;
 
 public class Export {
     public static void main(String[] args) throws IOException {
-        File apiJar = new File("./runelite-api/target/runelite-api-" + RuneLiteProperties.getVersion() + ".jar");
-        File clientJar = new File("./runelite-client/target/client-" + RuneLiteProperties.getVersion() + ".jar");
-        File hotliteDir = new File(System.getProperty("user.home") + "/.runelite/hotlite/" + RuneLiteProperties.getVersion() + "/");
+        String version = "1.10.17.2";
 
-        File targetApiJar = new File(hotliteDir, "runelite-api-" + RuneLiteProperties.getVersion() + ".jar");
-        File targetClientJar = new File(hotliteDir, "client-" + RuneLiteProperties.getVersion() + ".jar");
-        if (apiJar.exists() && clientJar.exists()) {
-            hotliteDir.mkdirs();
-            Files.copy(apiJar.toPath(), targetApiJar.toPath(), StandardCopyOption.REPLACE_EXISTING);
-            Files.copy(clientJar.toPath(), targetClientJar.toPath(), StandardCopyOption.REPLACE_EXISTING);
-            System.out.println("Success");
-        }
+        File hotliteDir = new File(System.getProperty("user.home") + "/.runelite/hotlite/" + version + "/");
+        hotliteDir.mkdirs();
+
+        File apiJar = new File("./runelite-api/target/runelite-api-" + version + ".jar");
+        File clientJar = new File("./runelite-client/target/client-" + version + ".jar");
+
+        if (!apiJar.exists())
+            throw new RuntimeException("API jar missing, Maven install first.");
+
+        if (!clientJar.exists())
+            throw new RuntimeException("Client jar missing, Maven install first.");
+
+        System.out.println("Hotlite Exporting " + version);
+
+        File targetApiJar = new File(hotliteDir, "runelite-api-" + version + ".jar");
+        File targetClientJar = new File(hotliteDir, "client-" + version + ".jar");
+
+        Files.copy(apiJar.toPath(), targetApiJar.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        Files.copy(clientJar.toPath(), targetClientJar.toPath(), StandardCopyOption.REPLACE_EXISTING);
     }
 }
